85013c27a1a61a2c8dd4e36bc16195a5fa23e8a2
groundbeeflover <gevegg22@gmail.com>
2025-06-09 19:58:42 +0200

    some setup

--- types/index.ts
+++ types/index.ts
@@ -12,3 +12,23 @@ export type Announcement = {
     date: string
     description: string
 }
+
+export type User = {
+    userid: string
+    displayName: string
+    password: BinaryType
+}
+
+export type Achievement = {
+    achievementID: BigInt
+    userID: string
+    achievementProgress: BigInt
+}
+
+export type AchievementType = {
+    achievementID: BigInt
+    description: string
+    difficulty: string
+    totalProgress: BigInt
+    name: String
+}
