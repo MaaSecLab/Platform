9e2af1af6e4f5d7b74970543ef851a6359060ce9
Arezim Vbk-Ubu <arezimt@gmail.com>
2025-07-02 15:48:20 +0200

    fix: Adjust the CSS on Announcements and Blog [slug]s so the footer always sticks to the bottom of the viewport on wider displays

--- src/app/(Pages)/Announcements/[slug]/page.tsx
+++ src/app/(Pages)/Announcements/[slug]/page.tsx
@@ -1,35 +1,42 @@
 import Link from "next/link";
-import {ArrowLeftIcon} from "lucide-react";
-import {getArticleData, getArticleSlugs} from "../../../../../lib/announcement";
+import { ArrowLeftIcon } from "lucide-react";
+import {
+  getArticleData,
+  getArticleSlugs,
+} from "../../../../../lib/announcement";
 import Footer from "app/components/Footer";
 
+const Announcement = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { slug } = await params;
+  const articleData = await getArticleData(slug);
 
-
-const Announcement = async ({params}: {params: Promise<{slug: string}>}) => {
-    const {slug} = await params;
-    const articleData = await getArticleData(slug)
-    
-    return (
-        <div className="bg-white">
-        <section className="mx-auto w-10/12 mt-20 flex flex-col gap-5 bg-white">
-            <div className="flex justify-between ">
-                <Link href="/Announcements" className="text-black">
-                    <ArrowLeftIcon width={20}/>
-                    <p>Announcements</p>
-                </Link>
-                <p className="text-black">{articleData.date}</p>
-            </div>
-            <article className="article" dangerouslySetInnerHTML={{__html: articleData.contentHtml}}/>
-        </section>
-        <Footer />
+  return (
+    <div className="min-h-screen bg-white flex flex-col">
+      <section className="mx-auto w-10/12 mt-20 flex flex-col gap-5 bg-white flex-1">
+        <div className="flex justify-between ">
+          <Link href="/Announcements" className="text-black">
+            <ArrowLeftIcon width={20} />
+            <p>Announcements</p>
+          </Link>
+          <p className="text-black">{articleData.date}</p>
         </div>
-
-)
-}
+        <article
+          className="article"
+          dangerouslySetInnerHTML={{ __html: articleData.contentHtml }}
+        />
+      </section>
+      <Footer />
+    </div>
+  );
+};
 
 export default Announcement;
 
 export async function generateStaticParams() {
-    const slugs = await getArticleSlugs();
-    return slugs.map((slug: string) => ({slug}));
+  const slugs = await getArticleSlugs();
+  return slugs.map((slug: string) => ({ slug }));
 }
--- src/app/(Pages)/Blog/[slug]/page.tsx
+++ src/app/(Pages)/Blog/[slug]/page.tsx
@@ -3,34 +3,34 @@ import { ArrowLeftIcon } from "lucide-react";
 import { getArticleData, getArticleSlugs } from "../../../../../lib/blogs";
 import Footer from "app/components/Footer";
 
+const Blog = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  //const params = await props.params;
+  const { slug } = await params;
+  const articleData = await getArticleData(slug);
 
-
-
-const Blog = async ({params}: { params: Promise<{slug: string}> }) => {
-    //const params = await props.params;
-    const {slug} = await params;
-    const articleData = await getArticleData(slug)
-
-    return (
-        <div>
-            <section className="mx-auto w-10/12 mt-20 flex flex-col gap-5">
-                <div className="flex justify-between ">
-                    <Link href="/Blog">
-                        <ArrowLeftIcon width={20} />
-                        <p>Blogs</p>
-                    </Link>
-                    <p>{articleData.date.toString()}</p>
-                </div>
-                <article className="article" dangerouslySetInnerHTML={{ __html: articleData.contentHtml }} />
-            </section>
-            <Footer />
+  return (
+    <div className="min-h-screen flex flex-col">
+      <section className="mx-auto w-10/12 mt-20 flex flex-col gap-5 flex-1">
+        <div className="flex justify-between ">
+          <Link href="/Blog">
+            <ArrowLeftIcon width={20} />
+            <p>Blogs</p>
+          </Link>
+          <p>{articleData.date.toString()}</p>
         </div>
-    )
-}
+        <article
+          className="article"
+          dangerouslySetInnerHTML={{ __html: articleData.contentHtml }}
+        />
+      </section>
+      <Footer />
+    </div>
+  );
+};
 
 export default Blog;
 
-export async function generateStaticParams(){
-    const slugs = await getArticleSlugs();
-    return slugs.map((slug: string) => ({slug}));
+export async function generateStaticParams() {
+  const slugs = await getArticleSlugs();
+  return slugs.map((slug: string) => ({ slug }));
 }
